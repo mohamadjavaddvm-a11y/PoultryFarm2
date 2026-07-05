49bd70fb17f10296983c97428ad326120a332195
mohamadjavaddvm-a11y <mohamadjavaddvm@gmail.com>
2026-07-05 20:19:22 +0330

    Create MainActivity.kt

---
+++ app/src/main/java/com/example/poultryfarm/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.poultryfarm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.poultryfarm.ui.AppNavigation
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    val viewModel: MainViewModel = viewModel()
+                    AppNavigation(viewModel)
+                }
+            }
+        }
+    }
+}
